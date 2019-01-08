3a04c063810740f246ab6d997e2c083870b0c375
caiosousa21 <caiofmsousa@gmail.com>
2019-01-08 13:39:02 -0200

    Mockup da Home page do projeto montada rusticamente

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import './App.css';
+import Header from './common/header'
+import Home from './home/index'
+
+const theme = createMuiTheme({
+  paltte: {
+    primary: {
+      main: '#e91e63',
+    },
+    secondary: {
+      main: '#00b0ff',
+    },
+  },
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <MuiThemeProvider theme={theme}>
+        <Header />
+        <Home />
+      </MuiThemeProvider>
+
+    )
   }
 }
 
