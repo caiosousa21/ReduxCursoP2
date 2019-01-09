312f9fff450de5f28a4b1587947a37edb08306ca
caiosousa21 <caiofmsousa@gmail.com>
2019-01-09 11:18:17 -0200

    index de CreateList conectado ao store, e função addProduct passada para Form através de props, o qual já está pegando os dados e atualizando o state da aplicação

--- src/createList/Form.js
+++ src/createList/Form.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import MenuItem from '@material-ui/core/MenuItem'
 import TextField from '@material-ui/core/TextField'
 import InputAdornment from '@material-ui/core/InputAdornment'
@@ -6,57 +6,97 @@ import Button from '@material-ui/core/Button'
 
 const units = ['kg', 'lt', 'un']
 
-const Form = () => (
-    <form className='form-container'>
-        <div className='form-row'>
-            <TextField
-                label='Lista'
-                name='list'
-                value={''}
-                onChange={() => { }}
-                required
-            />
-            <Button variant='outlined' color='secondary'>Adicionar</Button>
-        </div>
-        <div className='form-row'>
-            <TextField
-                label='Produto'
-                name='product'
-                value={''}
-                onChange={() => { }}
-                required
-            />
-            <TextField
-                label='Quantidade'
-                name='quantity'
-                value={''}
-                onChange={() => { }}
-                required
-            />
-            <TextField
-                select
-                label='Unidade'
-                name='unity'
-                value={''}
-                onChange={() => { }}
-                required
-            >
-                {units.map(option => (
-                    <MenuItem key={option} value={option}>{option}</MenuItem>
-                ))}
-            </TextField>
-            <TextField
-                label='Preço'
-                name='price'
-                value={''}
-                onChange={() => { }}
-                InputProps={{
-                    startAdornment: <InputAdornment position='start'>R$</InputAdornment>
-                }}
-            />
-        </div>
+class Form extends Component {
+    state = {
+        list: '',
+        product: '',
+        quantity: '',
+        unit: '',
+        price: '',
+        showErrors: false,
+    };
 
-    </form>
-)
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        const { list, product, quantity, unit, price } = this.state
+        if (!list || !product || !quantity || !unit || !price) {
+            this.setState({ showErrors: true })
+        } else {
+            this.props.addProduct({ product, quantity, unit, price }, list);
+            this.setState({
+                list: '',
+                product: '',
+                quantity: '',
+                unit: '',
+                price: '',
+                showErrors: false,
+            })
+        }
+    }
+
+    render() {
+        return (
+            <form className='form-container'>
+                <div className='form-row'>
+                    <TextField
+                        label='Lista'
+                        name='list'
+                        value={this.state.list}
+                        onChange={this.handleChange}
+                        required
+                        error={!this.state.list && this.state.showErrors}
+                    />
+                    <Button variant='outlined' onClick={this.handleSubmit} color='secondary'>Adicionar</Button>
+                </div>
+                <div className='form-row'>
+                    <TextField
+                        label='Produto'
+                        name='product'
+                        value={this.state.product}
+                        onChange={this.handleChange}
+                        required
+                        error={!this.state.product && this.state.showErrors}
+                    />
+                    <TextField
+                        label='Quantidade'
+                        name='quantity'
+                        value={this.state.quantity}
+                        onChange={this.handleChange}
+                        required
+                        error={!this.state.quantity && this.state.showErrors}
+                    />
+                    <TextField
+                        select
+                        label='Unidade'
+                        name='unit'
+                        value={this.state.unit}
+                        onChange={this.handleChange}
+                        required
+                        error={!this.state.unit && this.state.showErrors}
+                    >
+                        {units.map(option => (
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField
+                        label='Preço'
+                        name='price'
+                        value={this.state.price}
+                        onChange={this.handleChange}
+                        InputProps={{
+                            startAdornment: <InputAdornment position='start'>R$</InputAdornment>
+                        }}
+                    />
+                </div>
+
+            </form>
+        )
+    }
+}
 
 export default Form
--- src/createList/index.js
+++ src/createList/index.js
@@ -2,15 +2,31 @@ import React from 'react'
 import Form from './Form'
 import './List.css'
 import ListItem from './ListItem'
-const CreateList = () => (
-    <div className='page-container'>
-        <Form />
-        <div className='list-items-container'>
-            <ListItem />
-        </div>
-    </div>
-)
 
+import { connect } from 'react-redux'
+import { Creators as ListActions } from '../store/actions/list'
+import { bindActionCreators } from 'redux'
 
+class CreateList extends React.Component {
+    addProduct = (product, list) => {
+        this.props.addProduct(product, list)
+    }
+    render() {
+        return (
+            <div className='page-container'>
+                <Form addProduct={this.addProduct}/>
+                <div className='list-items-container'>
+                    <ListItem />
+                </div>
+            </div>
+        )
+    }
+}
 
-export default CreateList
+const mapStateToProps = state => ({
+    list: state.list
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators(ListActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateList)
